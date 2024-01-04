ed8ca7295c688574f30e7aa5c7edff30e0304e2a
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-04 21:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function validEmail(str) {
-  //your JS code here.
+function validEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email); 
 }
+ 
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
